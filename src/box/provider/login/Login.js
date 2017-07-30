601eaec2101727902f6cfc208892c175f8da01ec
igor-nm <igor.martucelli@hotmail.com>
2017-07-29 21:57:34 -0300

    Escope: NAVIGATION | Status: DONE

--- src/box/provider/login/Login.js
+++ src/box/provider/login/Login.js
@@ -4,6 +4,7 @@ import RaisedButton from 'material-ui/RaisedButton';
 import TextField from 'material-ui/TextField';
 import {Card} from 'material-ui/Card';
 import {Link} from 'react-router-dom';
+import history from '../../../service/Router';
 
 class Login extends Component {
 
@@ -24,7 +25,7 @@ class Login extends Component {
                 }
                 this.setState({msg:'falha ao realizar login!'});
             })
-            .then(sucess => this.setState({msg:'vosso token madame: '+ sucess.token}))
+            .then(sucess => {history.push('/provider/dash');})
             .catch(error => console.log(error));
     }
 
@@ -37,44 +38,46 @@ class Login extends Component {
 
     render() {
         return (
-            <div className="div-dad">
-                <Card style={{ padding: 20,}} >
-                    <div className="div-login">
-                        <h2 className="font">Já é aluno?</h2>
-                        <p className="font">Faça login e boa aula!</p>
-                        <TextField
-                        hintText="Email"
-                        floatingLabelText="Email"
-                        fullWidth={true}
-                        ref={(input) => { this.email = input; }}
-                        />
-                        <TextField
-                            hintText="Senha"
-                            floatingLabelText="Senha"
-                            type="password"
+            <div>
+                <div className="div-dad">
+                    <Card className="div-login">
+                        <div>
+                            <h2 className="font">Bem vindo professor!</h2>
+                            <p className="font">Vamos começar a aula?</p>
+                            <TextField
+                            hintText="Email"
+                            floatingLabelText="Email"
                             fullWidth={true}
-                            ref={(input) => { this.password = input; }}
-                        />
-                        <div className="div-action">
-                            <RaisedButton
-                                onTouchTap={this.makeLogin.bind(this)}
-                                label="Login" className="button" 
+                            ref={(input) => { this.email = input; }}
                             />
-                            <RaisedButton
-                                label="Login Facebook" className="button"
+                            <TextField
+                                hintText="Senha"
+                                floatingLabelText="Senha"
+                                type="password"
+                                fullWidth={true}
+                                ref={(input) => { this.password = input; }}
                             />
+                            <div className="div-action">
+                                <RaisedButton
+                                    onTouchTap={this.makeLogin.bind(this)}
+                                    label="Login" className="button" 
+                                />
+                                <RaisedButton
+                                    label="Login Facebook" className="button"
+                                />
+                            </div>
+                            <p className="font forgot-password"><Link to={"/"} className="forgot-password">Esqueci minha senha</Link></p>
+                            <span className="font">
+                                {this.state.msg}
+                            </span>
                         </div>
-                        <p className="font forgot-password"><Link to={"/"}>Esqueci minha senha</Link></p>
-                        <span style={{fontFamily: 'Roboto', fontSize: '20px',}}>
-                            {this.state.msg}
-                        </span>
-                    </div>
+                    </Card>
                     <div className="div-info">
                         Lorem ipsum elementum aliquet laoreet tempor lectus nullam, senectus ornare accumsan inceptos tempor leo suspendisse, sit augue hendrerit potenti per dolor. sit aenean vulputate porttitor tempus aliquam himenaeos primis augue lobortis, sapien iaculis blandit ipsum vulputate id iaculis suspendisse lobortis, rhoncus quisque lacus lobortis ipsum lorem nec aliquam. vestibulum dictumst potenti a mauris consectetur himenaeos eget cursus, quam id nisi praesent luctus adipiscing posuere, velit nec orci ornare tortor dictum at. amet laoreet lacus porttitor enim gravida fringilla orci senectus luctus, ligula tempus ut senectus fringilla accumsan quis semper nostra neque, nostra himenaeos nullam elementum fames curabitur metus et. 
 
                         Pharetra nullam curabitur cubilia urna lorem lobortis blandit donec, aenean etiam risus arcu nunc interdum ornare. fusce feugiat aliquam ac habitant nunc platea magna laoreet vehicula, quisque consequat semper taciti justo auctor aptent aenean ut quis, aliquam mi posuere primis phasellus lacinia nam conubia. massa metus consequat curabitur semper faucibus mollis etiam litora lacus bibendum, nunc arcu aliquam velit molestie imperdiet leo auctor vitae, sodales cursus varius justo interdum felis luctus scelerisque laoreet. venenatis ut nulla eros ut vestibulum iaculis platea, convallis tincidunt bibendum viverra quis eu ligula, lacinia dictumst quis torquent convallis ultricies. 
                     </div>
-                </Card>
+                </div>
             </div>
         );
     }
--- src/index.js
+++ src/index.js
@@ -2,26 +2,29 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import injectTapEventPlugin from 'react-tap-event-plugin';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
-import { Route, BrowserRouter } from 'react-router-dom'
+import { Router, Route, BrowserRouter } from 'react-router-dom'
 import registerServiceWorker from './registerServiceWorker';
 import App from './App';
 import LoginProvider from './box/provider/login/Login';
 import LoginStudent from './box/student/login/Login';
 import AboutProvider from './box/provider/about/About';
 import DashProvider from './box/provider/dash/Dash'
+import history from './service/Router';
 
 injectTapEventPlugin();
 
 const Main = () => (
     <MuiThemeProvider>
         <BrowserRouter>
-            <div>
-                <Route exact path='/' component={App} />
-                <Route path='/login/provider' component={LoginProvider} />
-                <Route path='/login/student' component={LoginStudent} />
-                <Route path='/provider/about' component={AboutProvider} />
-                <Route path='/provider/dash' component={DashProvider} />
-            </div>
+            <Router history={history}>
+                <div>
+                    <Route exact path='/' component={App} />
+                    <Route path='/login/provider' component={LoginProvider} />
+                    <Route path='/login/student' component={LoginStudent} />
+                    <Route path='/provider/about' component={AboutProvider} />
+                    <Route path='/provider/dash' component={DashProvider} />
+                </div>
+            </Router>
         </BrowserRouter>
     </MuiThemeProvider>
 )
