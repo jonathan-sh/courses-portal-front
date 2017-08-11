4a40335e2185fb116fdbde07d68441d5e1572d55
Jonathan <jonathan.henrique.smtp@gmail.com>
2017-08-10 21:38:58 -0300

    Escopo: CRIANDO-CURSO | Obs.: Implemento tarefa de criar curso

--- src/box/provider/marketing/Marketing.js
+++ src/box/provider/marketing/Marketing.js
@@ -1,5 +1,7 @@
 import React, {Component} from "react";
 import PubSub from 'pubsub-js';
+import TextField from 'material-ui/TextField';
+import RaisedButton from 'material-ui/RaisedButton';
 
 class Marketing extends Component {
 
@@ -14,7 +16,30 @@ class Marketing extends Component {
     render() {
         return (
             <div>
-
+                <TextField
+                    hintText="Cabeça"
+                    floatingLabelText="Cabeça"
+                    fullWidth={true}
+                    ref={(input) => { this.description = input; }}
+                />
+                <TextField
+                    hintText="Descriação"
+                    floatingLabelText="Descriação"
+                    fullWidth={true}
+                    ref={(input) => { this.description = input; }}
+                />
+                <RaisedButton
+                    label={"CATEGORIAS"}
+                    fullWidth={true}
+                    backgroundColor="#2dc7a2"
+                    labelStyle={{color: '#FFF'}}
+                    style={{marginTop: '10px'}}>
+                </RaisedButton>
+                <RaisedButton
+                    label="salvar"
+                    backgroundColor="#0ac752"
+                    labelStyle={{color: 'white'}}
+                    style={{float: 'right', margin: '20px 0 20px 20px'}}/>
             </div>
         )
     }
