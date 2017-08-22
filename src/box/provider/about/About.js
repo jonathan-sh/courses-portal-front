129c8cfa91a7eefe85f0e217a061924339c3baaa
Jonathan <jonathan.henrique.smtp@gmail.com>
2017-08-21 22:19:45 -0300

    Escobo: GRADE | STATUS: INICIAL

--- src/box/provider/about/About.js
+++ src/box/provider/about/About.js
@@ -9,37 +9,57 @@ class About extends Component {
     constructor() {
         super()
         this.httpService = new httpService();
-        this.state = { classConfirmPassword: 'none' }
+        this.state = { provider: {name:'', email:'',} }
     };
 
     componentDidMount(){
-        PubSub.publish('header-label',"Sobre")
+        PubSub.publish('header-label','Sobre');
+        this.findProvider();
     }
 
-    appearConfirmPassword = () =>
-    {
-        this.password.input.value.length >= 4 ?
-        (this.setState({classConfirmPassword: ''})) : ((this.setState({classConfirmPassword: 'none'})));
+    findProvider = ()=>{
+        this.httpService.get('/provider', localStorage.getItem('auth-token'))
+            .then(response => {
+                if (response.status !== 501 )
+                {
+                    return response.json();
+                }
+                throw new Error('Falha de autenticação.');
+            })
+            .then(success => {
+                this.setState({"provider":success});
+            })
+            .catch(error => { console.log(error);});
+
+    };
+
+    setData = (event, value, attribute)=>{
+        this.setState({"provider":{[attribute]:value}});
     };
 
     render() {
         return (
             <div>
                 <TextField
+                    id="email"
                     disabled={true}
                     hintText="Email"
                     floatingLabelText="Email"
                     fullWidth={true}
-                    ref={(input) => { this.email = input; }}
+                    value= {this.state.provider.email}
                 />
                 <TextField
+                    id="name"
                     hintText="Nome"
                     floatingLabelText="Nome"
                     fullWidth={true}
                     errorText={''}
+                    onChange={ (event, value) =>  this.setData(event, value, 'name')}
                     ref={(input) => { this.name = input; }}
+                    value= {this.state.provider.name}
                 />
                 <TextField
+                    id="password0"
                     hintText="Senha"
                     floatingLabelText="Senha"
                     type="password"
@@ -49,12 +69,12 @@ class About extends Component {
                     onChange={this.appearConfirmPassword}
                 />
                 <TextField
+                    id="password1"
                     hintText="Confirmar senha"
                     floatingLabelText="Confirmar senha"
                     type="password"
                     fullWidth={true}
                     errorText={''}
-                    style={{display: this.state.classConfirmPassword}}
                     ref={(input) => { this.confirmPassword = input; }}
                 />
                 <RaisedButton
@@ -62,6 +82,7 @@ class About extends Component {
                     backgroundColor="#0ac752"
                     labelStyle={{color: 'white'}}
                     keyboardFocused={true}
+                    onTouchTap={this.fncFindCourse}
                     style={{float: 'right', margin: '20px 0 20px 20px'}}/>
 
                 <br/>
