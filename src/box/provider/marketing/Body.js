558ee650e748ba2d477edb0e8d642a75ae6a61e7
igor-nm <igor.martucelli@hotmail.com>
2017-09-13 07:15:42 -0300

    Escopo: MARKETING-BODY | Status: Done | Obs.: Save, update and delete topic for about company

--- src/box/provider/marketing/Body.js
+++ src/box/provider/marketing/Body.js
@@ -6,7 +6,10 @@ import TextField from 'material-ui/TextField';
 import httpService from './../../../service/HttpService';
 import NewIco from 'material-ui/svg-icons/content/add';
 import DeleteIco from 'material-ui/svg-icons/content/delete-sweep';
+import EditIco from 'material-ui/svg-icons/content/create';
 import RaisedButton from 'material-ui/RaisedButton';
+import FlatButton from 'material-ui/FlatButton';
+import Snackbar from 'material-ui/Snackbar';
 
 class Body extends Component
 {
@@ -17,34 +20,86 @@ class Body extends Component
         this.state =
         {
             provider: JSON.parse(localStorage.getItem('provider')),
-            topic: {title: '', content: ''},
+            topic: {header: '', description: ''},
             topics: '',
             update: false,
-            errorsText: ''
-        }
+            errorsText: '',
+            labelButton: 'Adicionar tópico',
+            iconButton: <NewIco color="#FFF"/>,
+            btn: false,
+            indexTopic: null,
+            showAction: false,
+            messageAction: 'Test is success',
+        };
     }
+
+    componentDidMount()
+    {
+        this.fncListTopic();
+    }
+
+    fncListTopic = () =>
+    {
+        this.setState({'provider': JSON.parse(localStorage.getItem('provider'))});
+        let provider = this.state.provider;
+
+        if(provider.topics !== null)
+        {
+            let topics = provider.topics.map((topic, index) =>
+                <div key={index}>
+                    <RaisedButton
+                        label={topic.header}
+                        backgroundColor="#2dc7a2"
+                        disabled={this.state.btn}
+                        labelStyle={{color: '#FFF'}}
+                        style={{marginTop: '10px', width: '92.1%'}}
+                        onTouchTap= {(object, position) => this.switchAction(topic, index)}
+                    />
+                    <RaisedButton
+                        label="delete"
+                        backgroundColor="#ff2930"
+                        icon={<DeleteIco color="#FFF"/>}
+                        style={{marginLeft:'0.7%'}}
+                        labelStyle={{color: 'white'}}
+                        onTouchTap={(object, position, attribute) => this.fncHandleDelete(topic, index, 'topics')}
+                    />
+                </div>
+            );
+
+            this.setState({'topics':topics});
+        }
+    };
+
+    switchAction = (object, position) =>
+    {
+        const label = 'Editar tópico';
+        const icon = <EditIco color="#FFF"/>;
+
+        this.setState({'topic': object, 'labelButton': label, 'update':true, 'iconButton': icon, 'indexTopic': position});
+        this.fncListTopic();
+    };
 
     isValidationFields = () =>
     {
-        const errorTitle = 'Informe o título';
-        const errorContent = 'Informe o conteúdo';
+        const errorHeader = 'Informe o título';
+        const errorDescription = 'Informe o conteúdo';
         let errors =
             {
-                title:'',
-                content:'',
+                header:'',
+                description:'',
             };
 
         this.setState({'errorText':errors});
 
-        this.title.input.value === '' ?
-            (errors.title = errorTitle) : (errors.title = '');
+        this.header.input.value === '' ?
+            (errors.header = errorHeader) : (errors.header = '');
 
-        this.content.input.value === '' ?
-            (errors.content = errorContent) : (errors.content = '');
+        this.description.props.value === '' ?
+            (errors.description = errorDescription) : (errors.description = '');
 
         this.setState({'errorsText': errors});
 
-        if(errors.title === '' && errors.content === '')
+        if(errors.header === '' && errors.description === '')
         {
             return true;
         }
@@ -72,7 +127,9 @@ class Body extends Component
     {
         response.password = null;
         this.setState({'provider':response});
+        this.setState({'showAction':true});
         localStorage.setItem('provider', JSON.stringify(response));
+        this.clearFields();
         console.log('Success');
     };
 
@@ -83,64 +140,103 @@ class Body extends Component
         this.setState({'topic':topic});
     };
 
-    fncHandleSave = () =>
+    fncHandleSave = (event, attribute, position) =>
     {
         if(this.isValidationFields())
         {
-            this.clearFields();
+            let provider = this.state.provider;
+            const topic = this.state.topic;
+
+            if(!this.state.update && position === null)
+            {
+                if (provider[attribute] === null || provider[attribute] === undefined)
+                {
+                    provider[attribute] = [];
+                }
+                provider[attribute].push(topic);
+                this.setState({'messageAction': 'Tópico ' + topic.header + ' adicionado com sucesso!'});
+                this.setState({'provider': provider});
+                this.makeUpdateProvider();
+            }
+            else
+            {
+                provider[attribute][position] = topic;
+                this.setState({'messageAction': 'Tópico ' + topic.header + ' alterado com sucesso!'});
+                this.setState({'provider': provider});
+                this.makeUpdateProvider();
+            }
         }
     };
 
+    fncHandleDelete = (object, position, attribute) =>
+    {
+        let provider = this.state.provider;
+        provider[attribute].splice(position , 1);
+        this.setState({'messageAction':'Tópico ' + object.header + ' deletada com sucesso!'});
+        this.setState({'provider': provider});
+        this.makeUpdateProvider();
+    };
+
+    fncHandleCancel = () => this.clearFields();
+
     clearFields = () =>
     {
-        this.setState({ 'topic': {title:'', content: ''} });
+        const label = 'Adicionar tópico';
+        const topic = {header: '', description: ''};
+        const icon = <NewIco color="#FFF"/>;
+
+        this.setState({'topic': topic, 'labelButton': label, 'update':false, 'iconButton': icon, 'indexTopic': null});
+        this.fncListTopic();
     };
+
+    handleEvent = () => this.setState({'showAction': false});
 
     render()
     {
         return(
             <div style={{textAlign: 'left'}}>
                 <TextField
-                    id="title"
+                    id="header"
                     hintText="Título"
                     floatingLabelText="Título"
                     fullWidth={true}
-                    errorText={this.state.errorsText.title}
-                    value={this.state.topic.title}
-                    ref={(input) => { this.title = input; }}
-                    onChange={ (event, value) =>  this.setData(event, value, 'title')}
+                    errorText={this.state.errorsText.header}
+                    value={this.state.topic.header}
+                    ref={(input) => { this.header = input; }}
+                    onChange={ (event, value) =>  this.setData(event, value, 'header')}
                 />
                 <TextField
-                    id="content"
+                    id="description"
                     hintText="Conteúdo"
                     floatingLabelText="Conteúdo"
                     fullWidth={true}
-                    errorText={this.state.errorsText.content}
-                    value={this.state.topic.content}
-                    ref={(input) => { this.content = input; }}
-                    onChange={ (event, value) =>  this.setData(event, value, 'content')}
+                    multiLine={true}
+                    rowsMax={5}
+                    errorText={this.state.errorsText.description}
+                    value={this.state.topic.description}
+                    ref={(input) => { this.description = input; }}
+                    onChange={ (event, value) =>  this.setData(event, value, 'description')}
                 />
                 <RaisedButton
-                    label="adicinar tópico"
+                    label={this.state.labelButton}
                     backgroundColor="#0ac752"
-                    icon={<NewIco color="#FFF"/>}
+                    icon={this.state.iconButton}
                     labelStyle={{color: 'white'}}
+                    style={{float: 'right', margin: '20px 0 20px 10px'}}
+                    onTouchTap={(event, attribute, position) => this.fncHandleSave(event, 'topics', this.state.indexTopic)}
+                />
+                <FlatButton
+                    label="Cancelar"
+                    primary={true}
+                    onTouchTap={this.fncHandleCancel}
                     style={{float: 'right', margin: '20px 0 20px 20px'}}
-                    onTouchTap={() => this.fncHandleSave()}
                 />
-
-                <RaisedButton
-                    label="Titulo topico"
-                    backgroundColor="#2dc7a2"
-                    labelStyle={{color: '#FFF'}}
-                    style={{marginTop: '10px', width: '92.1%'}}
-                />
-                <RaisedButton
-                    label="delete"
-                    backgroundColor="#ff2930"
-                    icon={<DeleteIco color="#FFF"/>}
-                    style={{marginLeft:'0.7%'}}
-                    labelStyle={{color: 'white'}}
+                {this.state.topics}
+                <Snackbar
+                    open={this.state.showAction}
+                    message={this.state.messageAction}
+                    autoHideDuration={2000}
+                    onRequestClose={this.handleEvent}
                 />
             </div>
         );
