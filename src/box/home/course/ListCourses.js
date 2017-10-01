211e312b8afa4b5bc65d7b60782a9e6bb02ca046
igor-nm <igor.martucelli@hotmail.com>
2017-10-01 13:02:10 -0300

    Escopo: LIST_COURSES | Satatus: IN_PROGRESS

--- src/box/home/course/ListCourses.js
+++ src/box/home/course/ListCourses.js
@@ -11,6 +11,7 @@ import IconSearch from 'material-ui/svg-icons/action/search';
 import ArrowLeft from 'material-ui/svg-icons/hardware/keyboard-arrow-left';
 import ArrowRight from 'material-ui/svg-icons/hardware/keyboard-arrow-right';
 import srcImage from '../../../style/img/course-not-found-2.jpg';
+import FlatButton from 'material-ui/FlatButton';
 
 class ListPublic extends Component
 {
@@ -21,7 +22,8 @@ class ListPublic extends Component
         this.state =
         {
             boxComponent: '',
-            grade: JSON.parse(localStorage.getItem('grade'))
+            grade: JSON.parse(localStorage.getItem('grade')),
+            componentMove: []
         };
     };
 
@@ -33,54 +35,111 @@ class ListPublic extends Component
     createBoxComponent = () =>
     {
         const grade = this.state.grade;
+        let componentMove = {};
 
         let boxes = grade.map((grade, index) =>
-            <div key={index}>
-                <h2 className='title-box'>{grade.description} ...</h2>
+        {
+            componentMove[grade.description] = {firstItem: 0, lastItem: 5};
+
+            return <div key={index}>
+                <h2 className='title-box'>Cursos em {grade.description} ...</h2>
                 <div className='component-category'>
                     <IconButton
-                        style={{background: 'transparent', width: 64, height: 64, padding: 8, float:'left'}}
+                        style={{background: 'transparent', width: 64, height: 64, padding: 8, float: 'left'}}
                         iconStyle={{width: 48, height: 48}}
                         tooltip='Voltar'
-                        href={'#' + grade.description + '0'}>
+                        onClick={(event, object, action) => this.actionMove(event, grade, 'back')}>
                         <ArrowLeft color='#00bcd4'/>
                     </IconButton>
                     <div className="horizontal-scroll">
                     {
-                        this.createCardComponent(grade.courses)
+                        this.createCardComponent(grade)
                     }
                     </div>
                     <IconButton
                         style={{background: 'transparent', width: 64, height: 64, padding: 8, float: 'left'}}
                         iconStyle={{width: 48, height: 48}}
                         tooltip='Ir'
-                        href={'#' + grade.description + '9'}>
+                        onClick={(event, object, action) => this.actionMove(event, grade, 'go')}>
                         <ArrowRight color='#00bcd4'/>
                     </IconButton>
                 </div>
+                <Divider style={{
+                    width: '80.5%',
+                    marginLeft: '9.8%',
+                    marginTop: '1.5%',
+                    backgroundColor: 'rgba(224, 224, 224, 0.5)'
+                }}/>
             </div>
-        );
-
-        this.setState({'boxComponent': boxes});
+        });
+        
+        console.log(grade);
+        this.setState({'boxComponent': boxes, 'componentMove': componentMove});
     };
 
-    createCardComponent = (courses) =>
+    createCardComponent = (grade) =>
     {
+        let courses = grade.courses;
+
         return courses.map((card, index) =>
-            <Card id={card.name+index} key={index} style={{width: '18.35%', marginRight: '2%'}}>
+            <Card id={grade.description+index} key={index} style={{width: '18.35%', marginRight: '2%'}}>
                 <CardMedia>
                     <img src={srcImage} alt=''/>
                 </CardMedia>
-                <CardTitle style={{paddingBottom: '0%'}} titleStyle={{fontSize: '20px'}} title={card.name}/>
+                <CardTitle style={{paddingBottom: '0%'}} titleStyle={{fontSize: '20px', fontWeight: '300'}} title={card.name}/>
                 <CardText>
                     {card.objective}
                 </CardText>
                 <Divider />
                 <CardActions style={{textAlign:'right'}}>
-                    <h2 style={{color:'green', fontSize:'20px', padding:'3%', fontWeight: '500', margin:'0%'}}>{card.price} R$</h2>
+                    <FlatButton
+                        label="Acessar curso"
+                        primary={true}
+                    />
                 </CardActions>
             </Card>
         );
+    };
+
+    actionMove = (event, grade, action) =>
+    {
+        const id = '#' + grade.description;
+        let box = this.state.componentMove;
+        let move = this.state.componentMove[grade.description];
+        const step = 5;
+
+        if(action === 'go')
+        {
+            if(grade.courses.length < move.lastItem + step)
+            {
+                move.firstItem = grade.courses.length - step -1;
+                move.lastItem = grade.courses.length - 1;
+            }
+            else
+            {
+                move.firstItem += step;
+                move.lastItem += step;
+            }
+            window.location = id + move.lastItem;
+        }
+        else
+        {
+            if(move.firstItem - step < 0)
+            {
+                move.firstItem = 0;
+                move.lastItem = 5;
+            }
+            else
+            {
+                move.firstItem -= step;
+                move.lastItem -= step;
+            }
+
+            window.location = id + move.firstItem;
+        }
+
+        box[move] = move;
+        this.setState({'componentMove': box});
     };
 
     render()
