ea1f7706e31cd43e24686ab595251069afc85766
igor-nm <igor.martucelli@hotmail.com>
2018-01-04 23:18:17 -0200

    Scope: Dropzone | Implementation: The post-method was implemented with form-data, sending the file placed inside the dropzone component

--- src/service/Dropzone.js
+++ src/service/Dropzone.js
@@ -6,41 +6,41 @@ import React, {Component} from 'react';
 import DropzoneComponent from 'react-dropzone-component';
 import 'react-dropzone-component/styles/filepicker.css';
 import 'dropzone/dist/min/dropzone.min.css';
+import '../style/css/dropzone.css';
 import PubSub from 'pubsub-js';
-
+import FileService from './repository/FileService';
 
 class Dropzone extends Component
 {
-
     constructor(props)
     {
         super(props);
         this.state = {file: '', limitFile: props.limitFile};
 
-        // For a full list of possible configurations,
-        // please consult http://www.dropzonejs.com/#configuration
         this.djsConfig =
-        {
-            addRemoveLinks: true,
-            acceptedFiles: "image/jpeg, image/png, image/gif, application/pdf",
-            autoProcessQueue: false
-        };
+            {
+                addRemoveLinks: true,
+                acceptedFiles: "image/jpeg, image/png, image/gif, application/pdf, text/plain",
+                autoProcessQueue: false,
+                previewTemplate: "<div class=\"dz-preview dz-file-preview\">\n  <div class=\"dz-image\"><img data-dz-thumbnail /></div>\n  <div class=\"dz-details\">\n    <div class=\"dz-size\"><span data-dz-size></span></div>\n    <div class=\"dz-filename\"><span data-dz-name></span></div>\n  </div>\n  <div class=\"dz-progress\"><span class=\"dz-upload\" data-dz-uploadprogress></span></div>\n  <div class=\"dz-error-message\"><span data-dz-errormessage></span></div>\n  <div class=\"dz-success-mark\">\n    <svg width=\"54px\" height=\"54px\" viewBox=\"0 0 54 54\" version=\"1.1\" xmlns=\"http://www.w3.org/2000/svg\" xmlns:xlink=\"http://www.w3.org/1999/xlink\" xmlns:sketch=\"http://www.bohemiancoding.com/sketch/ns\">\n      <title>Check</title>\n      <defs></defs>\n      <g id=\"Page-1\" stroke=\"none\" stroke-width=\"1\" fill=\"none\" fill-rule=\"evenodd\" sketch:type=\"MSPage\">\n        <path d=\"M23.5,31.8431458 L17.5852419,25.9283877 C16.0248253,24.3679711 13.4910294,24.366835 11.9289322,25.9289322 C10.3700136,27.4878508 10.3665912,30.0234455 11.9283877,31.5852419 L20.4147581,40.0716123 C20.5133999,40.1702541 20.6159315,40.2626649 20.7218615,40.3488435 C22.2835669,41.8725651 24.794234,41.8626202 26.3461564,40.3106978 L43.3106978,23.3461564 C44.8771021,21.7797521 44.8758057,19.2483887 43.3137085,17.6862915 C41.7547899,16.1273729 39.2176035,16.1255422 37.6538436,17.6893022 L23.5,31.8431458 Z M27,53 C41.3594035,53 53,41.3594035 53,27 C53,12.6405965 41.3594035,1 27,1 C12.6405965,1 1,12.6405965 1,27 C1,41.3594035 12.6405965,53 27,53 Z\" id=\"Oval-2\" stroke-opacity=\"0.198794158\" stroke=\"#747474\" fill-opacity=\"0.816519475\" fill=\"#FFFFFF\" sketch:type=\"MSShapeGroup\"></path>\n      </g>\n    </svg>\n  </div>\n  <div class=\"dz-error-mark\">\n    <svg width=\"54px\" height=\"54px\" viewBox=\"0 0 54 54\" version=\"1.1\" xmlns=\"http://www.w3.org/2000/svg\" xmlns:xlink=\"http://www.w3.org/1999/xlink\" xmlns:sketch=\"http://www.bohemiancoding.com/sketch/ns\">\n      <title>Check</title>\n      <defs></defs>\n      <g id=\"Page-1\" stroke=\"none\" stroke-width=\"1\" fill=\"none\" fill-rule=\"evenodd\" sketch:type=\"MSPage\">\n        <path d=\"M23.5,31.8431458 L17.5852419,25.9283877 C16.0248253,24.3679711 13.4910294,24.366835 11.9289322,25.9289322 C10.3700136,27.4878508 10.3665912,30.0234455 11.9283877,31.5852419 L20.4147581,40.0716123 C20.5133999,40.1702541 20.6159315,40.2626649 20.7218615,40.3488435 C22.2835669,41.8725651 24.794234,41.8626202 26.3461564,40.3106978 L43.3106978,23.3461564 C44.8771021,21.7797521 44.8758057,19.2483887 43.3137085,17.6862915 C41.7547899,16.1273729 39.2176035,16.1255422 37.6538436,17.6893022 L23.5,31.8431458 Z M27,53 C41.3594035,53 53,41.3594035 53,27 C53,12.6405965 41.3594035,1 27,1 C12.6405965,1 1,12.6405965 1,27 C1,41.3594035 12.6405965,53 27,53 Z\" id=\"Oval-2\" stroke-opacity=\"0.198794158\" stroke=\"#747474\" fill-opacity=\"0.816519475\" fill=\"#FFFFFF\" sketch:type=\"MSShapeGroup\"></path>\n      </g>\n    </svg>\n  </div>\n</div>"
+            };
 
         this.componentConfig =
-        {
-            iconFiletypes: ['.jpg', '.png', '.gif', '.pdf'],
-            showFiletypeIcon: true,
-            postUrl: '/url-server-upload'
-        };
-
-        this.handlePost = this.handlePost.bind(this);
+            {
+                iconFiletypes: ['.jpg', '.png', '.gif', '.pdf', '.txt'],
+                showFiletypeIcon: true,
+                postUrl: 'http://localhost:4212/file'
+            };
 
         this.dropzone = null;
+    }
 
+    componentDidMount()
+    {
         PubSub.subscribe('dropzone-make-upload', this.handlePost);
     };
 
-    limitFileService()
+    limitFileService = () =>
     {
         if(this.state.limitFile === true)
         {
@@ -51,17 +51,27 @@ class Dropzone extends Component
         }
     };
 
-    handleFileAdded(file)
+    cleanDrop = () =>
+    {
+        this.dropzone.removeAllFiles(true);
+    };
+
+    handleFileAdded = (file) =>
     {
         console.log(this.dropzone);
         this.limitFileService();
         this.setState({file: file});
     };
 
-    handlePost()
+    handlePost = () =>
     {
-        alert("uppando ...");
+        console.log("Uploading ...");
         this.dropzone.processQueue();
+
+        FileService.make()
+            .post('/file', this.state.file)
+            .then(success => console.log('success'))
+            .catch(error => console.log(error));
     };
 
     render()
@@ -71,18 +81,17 @@ class Dropzone extends Component
 
         // For a list of all possible events (there are many), see README.md!
         const eventHandlers =
-        {
-            init: dz => this.dropzone = dz,
-            addedfile: this.handleFileAdded.bind(this)
-        };
+            {
+                init: dz => this.dropzone = dz,
+                addedfile: this.handleFileAdded,
+            };
 
         return (
-            <div style={{marginTop: '2%'}}>
+            <div>
                 <DropzoneComponent
                     config={config}
                     eventHandlers={eventHandlers}
-                    djsConfig={djsConfig}
-                />
+                    djsConfig={djsConfig} />
             </div>
         );
     }
